708fbab167e5f54bed9dfa0419c8d1cbe016208e
BluePuppy21 <67152577+BluePuppy21@users.noreply.github.com>
2021-02-25 15:00:36 -0500

    Netbeans
    Started Netbeans

---
+++ Code/RentAHouse/src/main/java/com/github/bluepuppy21/rentahouse/RentAHouseApplication.java
@@ -0,0 +1,17 @@
+package com.github.bluepuppy21.rentahouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentAHouseApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RentAHouseApplication.class, args);
+	}
+
+
+
+}
+
+
---
+++ Code/RentAHouse/src/test/java/com/github/bluepuppy21/RentAHouse/RentAHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bluepuppy21.RentAHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentAHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
